a5930b486c61bf8423bb31d4312ad1e462b4af96
J Alan Brogan <github@al-got-rhythm.net>
2014-12-22 20:18:54 +0000

    PEP 8 - line length

--- copycat/letter.py
+++ copycat/letter.py
@@ -15,13 +15,17 @@ class Letter(WorkspaceObject):
 
     def describe(self, position, length):
         if length == 1:
-            self.addDescription(slipnet.stringPositionCategory, slipnet.single)
+            self.addDescription(slipnet.stringPositionCategory,
+                                slipnet.single)
         if self.leftmost and length > 1:  # ? why check length ?
-            self.addDescription(slipnet.stringPositionCategory, slipnet.leftmost)
+            self.addDescription(slipnet.stringPositionCategory,
+                                slipnet.leftmost)
         if self.rightmost and length > 1:  # ? why check length ?
-            self.addDescription(slipnet.stringPositionCategory, slipnet.rightmost)
+            self.addDescription(slipnet.stringPositionCategory,
+                                slipnet.rightmost)
         if length > 2 and position * 2 == length + 1:
-            self.addDescription(slipnet.stringPositionCategory, slipnet.middle)
+            self.addDescription(slipnet.stringPositionCategory,
+                                slipnet.middle)
 
     def __repr__(self):
         return '<Letter: %s>' % self.__str__()
@@ -31,11 +35,12 @@ class Letter(WorkspaceObject):
             return ''
         i = self.leftIndex - 1
         if len(self.string) <= i:
-            raise ValueError('len(self.string) <= self.leftIndex :: %d <= %d' % (len(self.string), self.leftIndex))
+            raise ValueError('len(self.string) <= self.leftIndex :: %d <= %d',
+                             len(self.string), self.leftIndex)
         return self.string[i]
 
     def distinguishingDescriptor(self, descriptor):
-        """Whether no other object of the same type (letter) has the same descriptor"""
+        """Whether no other object of the same type has the same descriptor"""
         if not WorkspaceObject.distinguishingDescriptor(self, descriptor):
             return False
         for objekt in self.string.objects:
