fargonauts/copycat

commit daeff3d9bf55c7e88bdb9457ffb057b92eaea024

Pylint the code

copycat/coderack.py
@@ -16,6 +16,13 @@ MAX_NUMBER_OF_CODELETS = 100
 codeletsUsed = {}
 
 
+def getUrgencyBin(urgency):
+    i = int(urgency) * NUMBER_OF_BINS / 100
+    if i >= NUMBER_OF_BINS:
+        return NUMBER_OF_BINS
+    return i + 1
+
+
 class CodeRack(object):
     def __init__(self):
         #logging.debug('coderack.__init__()')
@@ -46,13 +53,6 @@ class CodeRack(object):
             self.postTopDownCodelets()
             self.postBottomUpCodelets()
 
-    def getUrgencyBin(self, urgency):
-        bin = int(urgency) * NUMBER_OF_BINS
-        bin /= 100
-        if bin >= NUMBER_OF_BINS:
-            bin = NUMBER_OF_BINS - 1
-        return bin + 1
-
     def post(self, codelet):
         self.postings[codelet.name] = self.postings.get(codelet.name, 0) + 1
         self.pressures.addCodelet(codelet)
@@ -71,10 +71,10 @@ class CodeRack(object):
                 probability = workspaceFormulas.probabilityOfPosting(
                     codeletName)
                 howMany = workspaceFormulas.howManyToPost(codeletName)
-                for unused in range(0, howMany):
+                for _ in range(0, howMany):
                     if random.random() >= probability:
                         continue
-                    urgency = self.getUrgencyBin(
+                    urgency = getUrgencyBin(
                         node.activation * node.conceptualDepth / 100.0)
                     codelet = Codelet(codeletName, urgency, self.codeletsRun)
                     codelet.arguments += [node]
@@ -108,7 +108,7 @@ class CodeRack(object):
             urgency = 1
         if formulas.Temperature < 25.0 and 'translator' in codeletName:
             urgency = 5
-        for unused in range(0, howMany):
+        for _ in range(0, howMany):
             if random.random() < probability:
                 codelet = Codelet(codeletName, urgency, self.codeletsRun)
                 self.post(codelet)
@@ -128,6 +128,7 @@ class CodeRack(object):
         newCodelet.pressure = oldCodelet.pressure
         self.tryRun(newCodelet)
 
+    # pylint: disable=too-many-arguments
     def proposeRule(self, facet, description, category, relation, oldCodelet):
         """Creates a proposed rule, and posts a rule-strength-tester codelet.
 
@@ -162,9 +163,9 @@ class CodeRack(object):
         numberOfMappings = len(mappings)
         if urgency:
             urgency /= numberOfMappings
-        bin = self.getUrgencyBin(urgency)
+        binn = self.getUrgencyBin(urgency)
         logging.info('urgency: %s, number: %d, bin: %d',
-                     urgency, numberOfMappings, bin)
+                     urgency, numberOfMappings, binn)
         self.newCodelet('correspondence-strength-tester',
                         oldCodelet, urgency, correspondence)
 
@@ -226,7 +227,7 @@ class CodeRack(object):
 
     def postInitialCodelets(self):
         for name in self.initialCodeletNames:
-            for unused in range(0, workspaceFormulas.numberOfObjects()):
+            for _ in range(0, workspaceFormulas.numberOfObjects()):
                 codelet = Codelet(name, 1, self.codeletsRun)
                 self.post(codelet)
                 codelet2 = Codelet(name, 1, self.codeletsRun)
@@ -296,8 +297,8 @@ class CodeRack(object):
         threshold = r * urgsum
         chosen = None
         urgencySum = 0.0
-        logging.info('temperature: %f' % formulas.Temperature)
-        logging.info('actualTemperature: %f' % formulas.actualTemperature)
+        logging.info('temperature: %f', formulas.Temperature)
+        logging.info('actualTemperature: %f', formulas.actualTemperature)
         logging.info('Slipnet:')
         for node in slipnet.slipnodes:
             logging.info("\tnode %s, activation: %d, buffer: %d, depth: %s",
@@ -305,7 +306,7 @@ class CodeRack(object):
                          node.conceptualDepth)
         logging.info('Coderack:')
         for codelet in self.codelets:
-            logging.info('\t%s, %d' % (codelet.name, codelet.urgency))
+            logging.info('\t%s, %d', codelet.name, codelet.urgency)
         from workspace import workspace
 
         workspace.initial.log("Initial: ")
@@ -344,7 +345,7 @@ class CodeRack(object):
                              methodName)
         if not callable(method):
             raise RuntimeError('Cannot call %s()' % methodName)
-        args, varargs, varkw, defaults = inspect.getargspec(method)
+        args, _varargs, _varkw, _defaults = inspect.getargspec(method)
         #global codeletsUsed
         #codeletsUsed[methodName] = codeletsUsed.get(methodName,0) + 1
         try:

copycat/coderackPressure.py
@@ -13,6 +13,48 @@ class CoderackPressure(object):
         self.codelets = []
 
 
+def _codelet_index(codelet):
+    name_indices = {
+        'bottom-up-bond-scout': 0,
+        'top-down-bond-scout--category': {
+            slipnet.successor: 1,
+            slipnet.predecessor: 2,
+            None: 3
+        },
+        'top-down-bond-scout--direction': {
+            slipnet.left: 4,
+            slipnet.right: 5,
+            None: 3,
+        },
+        'top-down-group-scout--category': {
+            slipnet.successorGroup: 6,
+            slipnet.predecessorGroup: 7,
+            None: 8,
+        },
+        'top-down-group-scout--direction': {
+            slipnet.left: 9,
+            slipnet.right: 10,
+            None: -1,
+        },
+        'group-scout--whole-string': 11,
+        'replacement-finder': 12,
+        'rule-scout': 13,
+        'rule-translator': 14,
+        'bottom-up-correspondence-scout': 15,
+        'important-object-correspondence-scout': 16,
+        'breaker': 17,
+    }
+    i = name_indices.get(codelet.name, -1)
+    try:
+        return int(i)
+    except ValueError:
+        try:
+            node = codelet.arguments[0]
+            return i[node]
+        except KeyError:
+            return i[None]
+
+
 class CoderackPressures(object):
     def __init__(self):
         #logging.debug('coderackPressures.__init__()')
@@ -70,62 +112,16 @@ class CoderackPressures(object):
 
     def addCodelet(self, codelet):
         node = None
-        i = -1
-        if codelet.name == 'bottom-up-bond-scout':
-            i = 0
-        if codelet.name == 'top-down-bond-scout--category':
-            node = codelet.arguments[0]
-            if node == slipnet.successor:
-                i = 1
-            elif node == slipnet.predecessor:
-                i = 2
-            else:
-                i = 3
-        if codelet.name == 'top-down-bond-scout--direction':
-            node = codelet.arguments[0]
-            if node == slipnet.left:
-                i = 4
-            elif node == slipnet.right:
-                i = 5
-            else:
-                i = 3
-        if codelet.name == 'top-down-group-scout--category':
-            node = codelet.arguments[0]
-            if node == slipnet.successorGroup:
-                i = 6
-            elif node == slipnet.predecessorGroup:
-                i = 7
-            else:
-                i = 8
-        if codelet.name == 'top-down-group-scout--direction':
-            node = codelet.arguments[0]
-            if node == slipnet.left:
-                i = 9
-            elif node == slipnet.right:
-                i = 10
-        if codelet.name == 'group-scout--whole-string':
-            i = 11
-        if codelet.name == 'replacement-finder':
-            i = 12
-        if codelet.name == 'rule-scout':
-            i = 13
-        if codelet.name == 'rule-translator':
-            i = 14
-        if codelet.name == 'bottom-up-correspondence-scout':
-            i = 15
-        if codelet.name == 'important-object-correspondence-scout':
-            i = 16
-        if codelet.name == 'breaker':
-            i = 17
+        i = _codelet_index(codelet)
         if i >= 0:
             self.pressures[i].codelets += [codelet]
         if codelet.pressure:
-            codelet.pressure.codelets += [codelet]  # XXX why do this
+            codelet.pressure.codelets += [codelet]
         if i >= 0:
-            codelet.pressure = self.pressures[i]     # when this is next?
-        logging.info('Add %s: %d' % (codelet.name, i))
+            codelet.pressure = self.pressures[i]
+        logging.info('Add %s: %d', codelet.name, i)
         if node:
-            logging.info('Node: %s' % node.name)
+            logging.info('Node: %s', node.name)
 
     def removeCodelet(self, codelet):
         self.removedCodelets += [codelet]

copycat/slipnet.py
@@ -5,6 +5,7 @@ from sliplink import Sliplink
 
 
 class SlipNet(object):
+    # pylint: disable=too-many-instance-attributes
     def __init__(self):
         logging.debug("SlipNet.__init__()")
         self.initiallyClampedSlipnodes = []
@@ -21,8 +22,8 @@ class SlipNet(object):
         return '<slipnet>'
 
     def setConceptualDepths(self, depth):
-        logging.debug('slipnet set all depths to %f' % depth)
-        [node.setConceptualDepth(depth) for node in self.slipnodes]
+        logging.debug('slipnet set all depths to %f', depth)
+        _ = [node.setConceptualDepth(depth) for node in self.slipnodes]
 
     def reset(self):
         logging.debug('slipnet.reset()')
@@ -36,8 +37,8 @@ class SlipNet(object):
         logging.debug('slipnet.update()')
         self.numberOfUpdates += 1
         if self.numberOfUpdates == 50:
-            [node.unclamp() for node in self.initiallyClampedSlipnodes]
-        [node.update() for node in self.slipnodes]
+            _ = [node.unclamp() for node in self.initiallyClampedSlipnodes]
+        _ = [node.update() for node in self.slipnodes]
         for node in self.slipnodes:
             node.spread_activation()
         for node in self.slipnodes:
@@ -46,6 +47,7 @@ class SlipNet(object):
             node.buffer = 0.0
 
     def __addInitialNodes(self):
+        # pylint: disable=too-many-statements
         self.slipnodes = []
         self.letters = []
         for c in 'abcdefghijklmnopqrstuvwxyz':
@@ -135,47 +137,49 @@ class SlipNet(object):
         self.__addCategoryLink(self.samenessGroup, self.letterCategory, 50.0)
         # lengths
         for number in self.numbers:
-            self.__addInstanceLink(self.length, number, 100.0)
-        self.__addNonSlipLink(self.predecessorGroup, self.length, length=95.0)
-        self.__addNonSlipLink(self.successorGroup, self.length, length=95.0)
-        self.__addNonSlipLink(self.samenessGroup, self.length, length=95.0)
-        # opposites
-        self.__addOppositeLink(self.first, self.last)
-        self.__addOppositeLink(self.leftmost, self.rightmost)
-        self.__addOppositeLink(self.left, self.right)
-        self.__addOppositeLink(self.successor, self.predecessor)
-        self.__addOppositeLink(self.successorGroup, self.predecessorGroup)
+            self.__addInstanceLink(self.length, number)
+        groups = [self.predecessorGroup, self.successorGroup,
+                  self.samenessGroup]
+        for group in groups:
+            self.__addNonSlipLink(group, self.length, length=95.0)
+        opposites = [(self.first, self.last), (self.leftmost, self.rightmost),
+                     (self.leftmost, self.rightmost), (self.left, self.right),
+                     (self.successor, self.predecessor),
+                     (self.successorGroup, self.predecessorGroup)]
+        for a, b in opposites:
+            self.__addOppositeLink(a, b)
         # properties
         self.__addPropertyLink(self.letters[0], self.first, 75.0)
         self.__addPropertyLink(self.letters[-1], self.last, 75.0)
-        # object categories
-        self.__addInstanceLink(self.objectCategory, self.letter, 100.0)
-        self.__addInstanceLink(self.objectCategory, self.group, 100.0)
-        # string positions
-        self.__addInstanceLink(
-            self.stringPositionCategory, self.leftmost, 100.0)
-        self.__addInstanceLink(
-            self.stringPositionCategory, self.rightmost, 100.0)
-        self.__addInstanceLink(self.stringPositionCategory, self.middle, 100.0)
-        self.__addInstanceLink(self.stringPositionCategory, self.single, 100.0)
-        self.__addInstanceLink(self.stringPositionCategory, self.whole, 100.0)
-        # alphabetic positions
-        self.__addInstanceLink(
-            self.alphabeticPositionCategory, self.first, 100.0)
-        self.__addInstanceLink(
-            self.alphabeticPositionCategory, self.last, 100.0)
-        # direction categories
-        self.__addInstanceLink(self.directionCategory, self.left, 100.0)
-        self.__addInstanceLink(self.directionCategory, self.right, 100.0)
-        # bond categories
-        self.__addInstanceLink(self.bondCategory, self.predecessor, 100.0)
-        self.__addInstanceLink(self.bondCategory, self.successor, 100.0)
-        self.__addInstanceLink(self.bondCategory, self.sameness, 100.0)
-        # group categories
-        self.__addInstanceLink(
-            self.groupCategory, self.predecessorGroup, 100.0)
-        self.__addInstanceLink(self.groupCategory, self.successorGroup, 100.0)
-        self.__addInstanceLink(self.groupCategory, self.samenessGroup, 100.0)
+        links = [
+            # object categories
+            (self.objectCategory, self.letter),
+            (self.objectCategory, self.group),
+            # string positions,
+            (self.stringPositionCategory, self.leftmost),
+            (self.stringPositionCategory, self.rightmost),
+            (self.stringPositionCategory, self.middle),
+            (self.stringPositionCategory, self.single),
+            (self.stringPositionCategory, self.whole),
+            # alphabetic positions,
+            (self.alphabeticPositionCategory, self.first),
+            (self.alphabeticPositionCategory, self.last),
+            # direction categories,
+            (self.directionCategory, self.left),
+            (self.directionCategory, self.right),
+            # bond categories,
+            (self.bondCategory, self.predecessor),
+            (self.bondCategory, self.successor),
+            (self.bondCategory, self.sameness),
+            # group categories
+            (self.groupCategory, self.predecessorGroup),
+            (self.groupCategory, self.successorGroup),
+            (self.groupCategory, self.samenessGroup),
+            # bond facets
+            (self.bondFacet, self.letterCategory),
+            (self.bondFacet, self.length)]
+        for a, b in links:
+            self.__addInstanceLink(a, b)
         # link bonds to their groups
         self.__addNonSlipLink(
             self.sameness, self.samenessGroup, label=self.groupCategory,
@@ -191,9 +195,6 @@ class SlipNet(object):
                               label=self.bondCategory, length=90.0)
         self.__addNonSlipLink(self.predecessorGroup, self.predecessor,
                               label=self.bondCategory, length=90.0)
-        # bond facets
-        self.__addInstanceLink(self.bondFacet, self.letterCategory, 100.0)
-        self.__addInstanceLink(self.bondFacet, self.length, 100.0)
         # letter category to length
         self.__addSlipLink(self.letterCategory, self.length, length=95.0)
         self.__addSlipLink(self.length, self.letterCategory, length=95.0)
@@ -235,7 +236,7 @@ class SlipNet(object):
         link = self.__addLink(source, destination, None, length)
         source.categoryLinks += [link]
 
-    def __addInstanceLink(self, source, destination, length):
+    def __addInstanceLink(self, source, destination, length=100.0):
         categoryLength = source.conceptualDepth - destination.conceptualDepth
         self.__addCategoryLink(destination, source, categoryLength)
         #noinspection PyArgumentEqualDefault

copycat/workspaceObject.py
@@ -5,7 +5,20 @@ from slipnet import slipnet
 from workspaceStructure import WorkspaceStructure
 
 
+def distinguishingDescriptor(descriptor):
+    """Whether no other object of the same type has the same descriptor"""
+    if descriptor == slipnet.letter:
+        return False
+    if descriptor == slipnet.group:
+        return False
+    for number in slipnet.numbers:
+        if number == descriptor:
+            return False
+    return True
+
+
 class WorkspaceObject(WorkspaceStructure):
+    # pylint: disable=too-many-instance-attributes
     def __init__(self, workspaceString):
         WorkspaceStructure.__init__(self)
         self.string = workspaceString
@@ -45,13 +58,13 @@ class WorkspaceObject(WorkspaceStructure):
 
     def addDescription(self, descriptionType, descriptor):
         description = Description(self, descriptionType, descriptor)
-        logging.info("Adding description: %s to %s" % (description, self))
+        logging.info("Adding description: %s to %s", description, self)
         self.descriptions += [description]
 
     def addDescriptions(self, descriptions):
         copy = descriptions[:]  # in case we add to our own descriptions
         for description in copy:
-            logging.info('might add: %s' % description)
+            logging.info('might add: %s', description)
             if not self.containsDescription(description):
                 self.addDescription(description.descriptionType,
                                     description.descriptor)
@@ -69,7 +82,7 @@ class WorkspaceObject(WorkspaceStructure):
         for bond in self.bonds:
             bondStrength += bond.totalStrength
         divisor = 6.0
-        if self.spansString():  # XXX then we have already returned
+        if self.spansString():  # then we have already returned
             divisor = 3.0
         return bondStrength / divisor
 
@@ -98,7 +111,6 @@ class WorkspaceObject(WorkspaceStructure):
         if self.correspondence:
             interStringHappiness = self.correspondence.totalStrength
         self.interStringUnhappiness = 100.0 - interStringHappiness
-        #logging.info("Unhappy: %s"%self.interStringUnhappiness)
 
         averageHappiness = (intraStringHappiness + interStringHappiness) / 2
         self.totalUnhappiness = 100.0 - averageHappiness
@@ -116,9 +128,9 @@ class WorkspaceObject(WorkspaceStructure):
                 (self.interStringUnhappiness, 0.2)))
         self.totalSalience = (self.intraStringSalience +
                               self.interStringSalience) / 2.0
-        logging.info('Set salience of %s to %f = (%f + %f)/2' % (
-            self.__str__(), self.totalSalience,
-            self.intraStringSalience, self.interStringSalience))
+        logging.info('Set salience of %s to %f = (%f + %f)/2',
+                     self.__str__(), self.totalSalience,
+                     self.intraStringSalience, self.interStringSalience)
 
     def isWithin(self, other):
         return (self.leftIndex >= other.leftIndex and
@@ -128,11 +140,8 @@ class WorkspaceObject(WorkspaceStructure):
         return [d for d in self.descriptions
                 if d.descriptionType.fully_active()]
 
-    def morePossibleDescriptions(self, node):
-        return []
-
     def getPossibleDescriptions(self, descriptionType):
-        logging.info('getting possible descriptions for %s' % self)
+        logging.info('getting possible descriptions for %s', self)
         descriptions = []
         from group import Group
         for link in descriptionType.instanceLinks:
@@ -168,7 +177,7 @@ class WorkspaceObject(WorkspaceStructure):
         return bool([d for d in self.descriptions if d.descriptor == slipnode])
 
     def middleObject(self):
-        # XXX only works if string is 3 chars long
+        # only works if string is 3 chars long
         # as we have access to the string, why not just " == len / 2" ?
         objectOnMyRightIsRightmost = objectOnMyLeftIsLeftmost = False
         for objekt in self.string.objects:
@@ -178,29 +187,18 @@ class WorkspaceObject(WorkspaceStructure):
                 objectOnMyRightIsRightmost = True
         return objectOnMyRightIsRightmost and objectOnMyLeftIsLeftmost
 
-    def distinguishingDescriptor(self, descriptor):
-        """Whether no other object of the same type has the same descriptor"""
-        if descriptor == slipnet.letter:
-            return False
-        if descriptor == slipnet.group:
-            return False
-        for number in slipnet.numbers:
-            if number == descriptor:
-                return False
-        return True
-
     def relevantDistinguishingDescriptors(self):
         return [d.descriptor
                 for d in self.relevantDescriptions()
-                if self.distinguishingDescriptor(d.descriptor)]
+                if distinguishingDescriptor(d.descriptor)]
 
     def getDescriptor(self, descriptionType):
         """The description attached to this object of the description type."""
         descriptor = None
-        logging.info("\nIn %s, trying for type: %s" % (
-            self, descriptionType.get_name()))
+        logging.info("\nIn %s, trying for type: %s",
+                     self, descriptionType.get_name())
         for description in self.descriptions:
-            logging.info("Trying description: %s" % description)
+            logging.info("Trying description: %s", description)
             if description.descriptionType == descriptionType:
                 return description.descriptor
         return descriptor

commit aeb8cda755c3bda227fc6001940c875119cb38f9

Tidy references (which were broken by daeff3d) #5

copycat/codeletMethods.py
@@ -4,7 +4,7 @@ import random
 import logging
 
 
-import slipnet
+from slipnet import slipnet
 import temperature
 import formulas
 from workspaceFormulas import chooseDirectedNeighbor

copycat/coderack.py
@@ -119,7 +119,7 @@ class CodeRack(object):
 
     def newCodelet(self, name, oldCodelet, strength, arguments=None):
         #logging.debug('Posting new codelet called %s' % name)
-        urgency = self.getUrgencyBin(strength)
+        urgency = getUrgencyBin(strength)
         newCodelet = Codelet(name, urgency, self.codeletsRun)
         if arguments:
             newCodelet.arguments = [arguments]
@@ -163,7 +163,7 @@ class CodeRack(object):
         numberOfMappings = len(mappings)
         if urgency:
             urgency /= numberOfMappings
-        binn = self.getUrgencyBin(urgency)
+        binn = getUrgencyBin(urgency)
         logging.info('urgency: %s, number: %d, bin: %d',
                      urgency, numberOfMappings, binn)
         self.newCodelet('correspondence-strength-tester',

copycat/coderackPressure.py
@@ -47,7 +47,7 @@ def _codelet_index(codelet):
     i = name_indices.get(codelet.name, -1)
     try:
         return int(i)
-    except ValueError:
+    except (TypeError, ValueError):
         try:
             node = codelet.arguments[0]
             return i[node]

copycat/slipnet.py
@@ -4,6 +4,18 @@ from slipnode import Slipnode
 from sliplink import Sliplink
 
 
+def distinguishingDescriptor(descriptor):
+    """Whether no other object of the same type has the same descriptor"""
+    if descriptor == slipnet.letter:
+        return False
+    if descriptor == slipnet.group:
+        return False
+    for number in slipnet.numbers:
+        if number == descriptor:
+            return False
+    return True
+
+
 class SlipNet(object):
     # pylint: disable=too-many-instance-attributes
     def __init__(self):

copycat/workspaceObject.py
@@ -1,20 +1,8 @@
 import logging
 
 from description import Description
-from slipnet import slipnet
+from slipnet import slipnet, distinguishingDescriptor
 from workspaceStructure import WorkspaceStructure
-
-
-def distinguishingDescriptor(descriptor):
-    """Whether no other object of the same type has the same descriptor"""
-    if descriptor == slipnet.letter:
-        return False
-    if descriptor == slipnet.group:
-        return False
-    for number in slipnet.numbers:
-        if number == descriptor:
-            return False
-    return True
 
 
 class WorkspaceObject(WorkspaceStructure):
@@ -22,7 +10,6 @@ class WorkspaceObject(WorkspaceStructure):
     def __init__(self, workspaceString):
         WorkspaceStructure.__init__(self)
         self.string = workspaceString
-        #self.string.objects += [ self ]
         self.descriptions = []
         self.extrinsicDescriptions = []
         self.incomingBonds = []
@@ -186,6 +173,10 @@ class WorkspaceObject(WorkspaceStructure):
             if objekt.rightmost and objekt.leftIndex == self.rightIndex + 1:
                 objectOnMyRightIsRightmost = True
         return objectOnMyRightIsRightmost and objectOnMyLeftIsLeftmost
+
+    def distinguishingDescriptor(self, descriptor):
+        # pylint: disable=no-self-use
+        return distinguishingDescriptor(descriptor)
 
     def relevantDistinguishingDescriptors(self):
         return [d.descriptor