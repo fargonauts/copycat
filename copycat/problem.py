bd8bec2d37e3e7d09d1f1e2ede3e74c420177a9f
LSaldyt <lucassaldyt@gmail.com>
2017-11-16 08:45:11 -0700

    WIP: Add cross-chi-2 tests

--- copycat/problem.py
+++ copycat/problem.py
@@ -3,29 +3,34 @@ from .copycat import Copycat
 from pprint import pprint
 
 class Problem:
-    def __init__(self, initial, modified, target, iterations, distributions, formulas=None):
+    def __init__(self, initial, modified, target, iterations, distributions=None, formulas=None):
         self.initial  = initial
         self.modified = modified
         self.target   = target
 
         self.iterations    = iterations
-        self.distributions = distributions
+        if distributions is None:
+            self.distributions = self.solve()
+        else:
+            self.distributions = distributions
         self.formulas      = formulas
         if formulas is not None:
             assert hasattr(Copycat().workspace, 'temperature')
 
-    def test(self, comparison):
+    def test(self, comparison, expected=None):
         print('-' * 120)
         print('Testing copycat problem: {} : {} :: {} : _'.format(self.initial,
                                                                   self.modified,
                                                                   self.target))
         print('expected:')
-        pprint(self.distributions)
+        if expected is None:
+            expected = self.distributions
+        pprint(expected)
 
         actual = self.solve()
         print('actual:')
         pprint(actual)
-        comparison(actual, self.distributions)
+        comparison(actual, expected)
         print('-' * 120)
 
     def solve(self):
--- tests.py
+++ tests.py
@@ -6,69 +6,25 @@ import sys
 
 from pprint  import pprint
 from copycat import Problem
+from copycat.statistics import iso_chi_squared
 
 # TODO: update test cases to use entropy
-
-# CHI2 values for n degrees freedom
-_chiSquared_table = {
-        1:3.841,
-        2:5.991,
-        3:7.815,
-        4:9.488,
-        5:11.071,
-        6:12.592,
-        7:14.067,
-        8:15.507,
-        9:16.919,
-        10:18.307
-        }
-
-class ChiSquaredException(Exception):
-    pass
-
-def chi_squared(actualDict, expectedDict):
-    for key in expectedDict.keys():
-        assert key in actualDict, 'The key {} was not tested'.format(key)
-        actual   = actualDict[key]
-        expected = expectedDict[key]
-
-        answerKeys = set(list(actual.keys()) + list(expected.keys()))
-        degreesFreedom = len(answerKeys)
-        chiSquared = 0
-
-        get_count = lambda k, d : d[k]['count'] if k in d else 0
-
-        for k in answerKeys:
-            E = get_count(k, expected)
-            O = get_count(k, actual)
-            if E == 0:
-                print('Warning! Expected 0 counts of {}, but got {}'.format(k, O))
-            else:
-                chiSquared += (O - E) ** 2 / E
-
-        if chiSquared >= _chiSquared_table[degreesFreedom]:
-            raise ChiSquaredException('Significant difference between expected and actual answer distributions: \n' +
-                'Chi2 value: {} with {} degrees of freedom'.format(chiSquared, degreesFreedom))
 
 def generate():
     print('Generating distributions for new file')
     iterations = 30
-    distributions = [
-            Problem('abc', 'abd', 'efg',    iterations, None),
-            Problem('abc', 'abd', 'ijk',    iterations, None),
-            Problem('abc', 'abd', 'xyz',    iterations, None),
-            Problem('abc', 'abd', 'ijkk',   iterations, None),
-            Problem('abc', 'abd', 'mrrjjj', iterations, None)]
-
-    for distribution in distributions:
-        distribution.generate()
+    problems = [
+            Problem('abc', 'abd', 'efg',    iterations),
+            Problem('abc', 'abd', 'ijk',    iterations),
+            Problem('abc', 'abd', 'xyz',    iterations),
+            Problem('abc', 'abd', 'ijkk',   iterations),
+            Problem('abc', 'abd', 'mrrjjj', iterations)]
 
     with open(TestCopycat.Filename, 'wb') as outfile:
-        pickle.dump(distributions, outfile)
-    return distributions
-        
-class TestCopycat(unittest.TestCase):
+        pickle.dump(problems, outfile)
+    return problems
 
+class TestCopycat(unittest.TestCase):
     Filename = None
 
     def setUp(self):
@@ -78,14 +34,14 @@ class TestCopycat(unittest.TestCase):
         print('Testing copycat with input file: {}'.format(TestCopycat.Filename))
         try:
             with open(TestCopycat.Filename, 'rb') as infile:
-                distributions = pickle.load(infile)
+                problems = pickle.load(infile)
         except Exception as e:
             print('Generating due to error:')
             print(e)
-            distributions = generate()
+            problems = generate()
 
-        for distribution in distributions:
-            distribution.test(chi_squared)
+        for problem in problems:
+            problem.test(iso_chi_squared)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
